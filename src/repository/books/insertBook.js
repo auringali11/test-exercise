850ca3780a5c9aab0d4bf2e2f28ac93a54ef665f
auringali11 <auringalieva@bk.ru>
2022-09-25 03:29:59 +0500

    refactor: minor changes

--- src/repository/books/insertBook.js
+++ src/repository/books/insertBook.js
@@ -2,8 +2,8 @@ const pool = require('../db/pool');
 
 module.exports = {
     insertBook: async (data) => {
-        const book = await pool.QueryRow('INSERT INTO books(title, author, user_id, created_at)  \
-         VALUES ($1, $2, $3, $4) RETURNING *', [data.title, data.author, data.user_id, data.created_at]);
+        const book = await pool.QueryRow('INSERT INTO books(title, author, user_id)  \
+         VALUES ($1, $2, $3) RETURNING *', [data.title, data.author, data.user_id]);
         return book;
     },
 };
